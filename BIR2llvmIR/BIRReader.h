7a419b0411c0111450496abae501cfd3995b0548
shubhamnarlawar <shubham.narlawar@rrlogic.co.in>
2020-08-06 22:45:38 +0530

    Indent BIRReader.h

--- BIR2llvmIR/BIRReader.h
+++ BIR2llvmIR/BIRReader.h
@@ -46,10 +46,10 @@ class BIRReader {
 };
 
 struct BinTypeDecl {
-    int         tag;
-    struct BinTypeSymbol  typeSym;
-    string      name;
-    int         flags;
+    int                    tag;
+    struct BinTypeSymbol   typeSym;
+    string                 name;
+    int                    flags;
 };
 
 struct BinTerminatorInsn {
@@ -58,46 +58,46 @@ struct BinTerminatorInsn {
 };
 
 struct BinBasicBlock {
-    string                    id;
-    vector<struct BinNonTerminatorInsn> instructions;
-    struct BinTerminatorInsn            terminator;
+    string                                id;
+    vector<struct BinNonTerminatorInsn>   instructions;
+    struct BinTerminatorInsn              terminator;
 };
 
 struct BinVarDecl {
-    struct BinTypeDecl      type;
-    string        name;
-    string        metaVarName;
-    struct BinVarKind       kind;
-    struct BinVarScope      scope;
-    bool          ignoreVariable;
-    struct BinBasicBlock    endBB;
-    struct BinBasicBlock    startBB;
-    int           insOffset;
+    struct BinTypeDecl     type;
+    string                 name;
+    string                 metaVarName;
+    struct BinVarKind      kind;
+    struct BinVarScope     scope;
+    bool                   ignoreVariable;
+    struct BinBasicBlock   endBB;
+    struct BinBasicBlock   startBB;
+    int                    insOffset;
 };
 
 struct BinParam {
-    string    name;
-    int       flags;
+    string   name;
+    int      flags;
 };
 
 struct BinInvokableType {
-    vector<struct BinTypeDecl>  paramTypes;
-    TypeDecl          restType;
-    TypeDecl          returnType;
+    vector<struct BinTypeDecl> paramTypes;
+    TypeDecl                   restType;
+    TypeDecl                   returnType;
 };
 
 struct BinBIRFunction {
-    string              name;
-    string              workerName;
-    int                 flags;
-    InvokableType       type;
-    vector<struct BinParam>       requiredParams;
-    VarDecl             receiver;
-    Param               restParam;
-    int                 paramCount;
-    vector<struct BinVarDecl>     localVars;
-    VarDecl             returnVar;
-    vector<struct BinBasicBlock>  basicBlocks;
+    string                       name;
+    string                       workerName;
+    int                          flags;
+    InvokableType                type;
+    vector<struct BinParam>      requiredParams;
+    VarDecl                      receiver;
+    Param                        restParam;
+    int                          paramCount;
+    vector<struct BinVarDecl>    localVars;
+    VarDecl                      returnVar;
+    vector<struct BinBasicBlock> basicBlocks;
 
     map<FuncParam, vector<BasicBlock>>   params;
     friend istream &operator >> (istream &is, struct BinBIRFunction &readBIRFunction) {
