186c4c231065caf8c3d52c8402248dbe91733301
shubhamnarlawar <shubham.narlawar@rrlogic.co.in>
2020-08-17 23:20:01 +0530

    Edit driver.cpp for reading BIR Package
    State of code - Non compilable

--- BIR2llvmIR/driver.cpp
+++ BIR2llvmIR/driver.cpp
@@ -16,5 +16,25 @@ int main()
         size = (int)dataInStream.tellg();
         dataInStream.seekg (0, ios::beg);
 
+	struct BinBIRPackage readBIRPackage;
+	char buf;
+	std::string org;
+        std::string name;
+        std::string value;
+
+	dataInStream.read((char*)&buf, sizeof(buf));
+	for (int i = 0; i < (int)buf; i++)
+		org += buf;
+	dataInStream.read((char*)&buf, sizeof(buf));
+	for (int i = 0; i < (int)buf; i++)
+		name += buf;
+	dataInStream.read((char*)&buf, sizeof(buf));
+	for (int i = 0; i < (int)buf; i++)
+		value += buf;
+
+	readBIRPackage.org = org;
+	readBIRPackage.name = name;
+	readBIRPackage.value = value;
+
 	//Reader.deserialize(Reader->dataInStream);
 }
