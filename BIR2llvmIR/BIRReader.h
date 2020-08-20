dfe528b1d28ca321b03c1ee9a18b12f152345d68
shubhamnarlawar <shubham.narlawar@rrlogic.co.in>
2020-08-20 09:44:30 +0530

    Add code for reading packages in BIRReader.h
    State of code - Non compilable

--- BIR2llvmIR/BIRReader.h
+++ BIR2llvmIR/BIRReader.h
@@ -1,69 +1,15 @@
 #ifndef BIRREADER_H
 #define BIRREADER_H
 
-#include <istream>
-#include "BIR.h"
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<vector>
+using namespace std;
+//#include "BIR.h"
 
-class BIRReader {
-  private:
-  string   filename;
+struct BinBIRPackage readBIRPackage;
 
-  public:
-    BIRReader();
-    BIRReader(string name); // This should open the file and initialize the
-                            // istream
-    ~BIRReader();
-
-    string name()  { return filename; }
-    char readByte();
-    void deserialize(istream *dataInStream) {
-
-        // Read import packages.
-        void readPackages(dataInStream);
-
-	// Read constants.
-        void readConstants(dataInStream) {}
-
-        // Read typeDescRef definitions.
-        void readtypeDescRef(dataInStream) {}
-
-        // Read package level variables.
-        void readVariables(dataInStream) {}
-
-        void readTypeDefBodies(dataInStream) {}
-
-        // Read functions.
-        void readFunctions(dataInStream) {}
-
-        // Read annotations.
-        void readAnnotations(dataInStream) {}
-    }
-
-    void readPackages(istream dataInStream) {
-      int symbolCount;
-      dataInStream >> symbolCount;
-      for (int i = 0; i < symbolCount; i++) {
-            dataInStream >> BinBIRPackage;
-      }
-    }
-
-    /*void readFunctions(istream *dataInStream) {
-      int symbolCount;
-      dataInStream >> symbolCount;
-      for (int i = 0; i < symbolCount; i++) {
-            dataInStream >> BinBIRFunction;
-      }
-    }*/
-
-  private:
-    BIRFunction readFunction();
-    BasicBlock readBasicBlock();
-    NonTerminatingInsn readNonTerminatingInsn();
-    TerminatingInsn readTerminatingInsn();
-    VarDecl readVarDecl();
-    Param readParam();
-    Symbol readSymbol();
-};
 /*
 struct BinTypeDecl {
     int                    tag;
@@ -127,18 +73,94 @@ struct BinBIRFunction {
 };*/
 
 struct BinBIRPackage {
-    string org;
-    string name;
-    string version;
-    string sourceFileName;
+    std::string org;
+    std::string name;
+    std::string version;
+    std::string sourceFileName;
 
     //vector<struct BinBIRFunction> functions;
-    friend ifstream &operator >> (ifstream &is, struct BinBIRPackage &readBIRPackage) {
-        is >> readBIRPackage.org;
-	is >> readBIRPackage.name;
-	is >> readBIRPackage.version;
-        return is;
+};
+
+class BIRReader {
+  private:
+  std::string   filename;
+
+  public:
+    BIRReader();
+    BIRReader(std::string name); // This should open the file and initialize the
+                            // istream
+    ~BIRReader();
+
+    std::string name()  { return filename; }
+    char readByte();
+    void deserialize() {
+
+        // Code copied from bir_t::constant_pool_set_t::_read() L90
+        // Purpose - read total pool count
+        m_constant_pool_count = m__io->read_s4be();
+        int l_constant_pool_entries = constant_pool_count();
+        m_constant_pool_entries = new std::vector<constant_pool_entry_t*>();
+        m_constant_pool_entries->reserve(l_constant_pool_entries);
+        for (int i = 0; i < l_constant_pool_entries; i++) {
+            m_constant_pool_entries->push_back(new constant_pool_entry_t(m__io, this, m__root));
+        }
+
+        ifstream dataInStream;
+        int size = 0;
+        dataInStream.open("main-bir-dump", ios::in|ios::binary);
+        dataInStream.seekg(0, ios::end);
+        size = (int)dataInStream.tellg();
+        dataInStream.seekg (0, ios::beg);
+
+        // Read import packages.
+
+        readBIRPackage.org = m__io->read_s4be();
+        readBIRPackage.name = m__io->read_s4be();
+        readBIRPackage.value = m__io->read_s4be();
+        //void readPackages(dataInStream);
+
+        // Read constants.
+        void readConstants(dataInStream);
+
+        // Read typeDescRef definitions.
+        void readtypeDescRef(dataInStream);
+
+        // Read package level variables.
+        void readVariables(dataInStream);
+
+        void readTypeDefBodies(dataInStream);
+
+        // Read functions.
+        void readFunctions(dataInStream);
+
+        // Read annotations.
+        void readAnnotations(dataInStream);
     }
+
+    void readPackages(ifstream dataInStream) {
+      int symbolCount;
+      dataInStream >> symbolCount;
+      for (int i = 0; i < symbolCount; i++) {
+            dataInStream >> BinBIRPackage;
+      }
+    }
+
+    /*void readFunctions(istream *dataInStream) {
+      int symbolCount;
+      dataInStream >> symbolCount;
+      for (int i = 0; i < symbolCount; i++) {
+            dataInStream >> BinBIRFunction;
+      }
+    }
+
+  private:
+    BIRFunction readFunction();
+    BasicBlock readBasicBlock();
+    NonTerminatingInsn readNonTerminatingInsn();
+    TerminatingInsn readTerminatingInsn();
+    VarDecl readVarDecl();
+    Param readParam();
+    Symbol readSymbol();*/
 };
 
 #endif // BIRREADER_H
