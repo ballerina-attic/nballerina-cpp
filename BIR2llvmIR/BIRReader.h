326ab6baa2f2a18041c7c57ef6cc2586aaea56ed
shubhamnarlawar <shubham.narlawar@rrlogic.co.in>
2020-08-24 22:57:44 +0530

    Add support for reading Constant Pool Entries from BIR dump
    State of Code - Compilable

--- BIR2llvmIR/BIRReader.h
+++ BIR2llvmIR/BIRReader.h
@@ -8,7 +8,137 @@
 using namespace std;
 //#include "BIR.h"
 
-struct BinBIRPackage readBIRPackage;
+enum tag_enum_t {
+    TAG_ENUM_CP_ENTRY_INTEGER = 1,
+    TAG_ENUM_CP_ENTRY_FLOAT = 2,
+    TAG_ENUM_CP_ENTRY_BOOLEAN = 3,
+    TAG_ENUM_CP_ENTRY_STRING = 4,
+    TAG_ENUM_CP_ENTRY_PACKAGE = 5,
+    TAG_ENUM_CP_ENTRY_BYTE = 6,
+    TAG_ENUM_CP_ENTRY_SHAPE = 7
+};
+
+std::ifstream is;
+
+// Read 1 byte from the stream
+uint8_t read_u1() {
+    uint8_t value;
+    is.read(reinterpret_cast<char *>(&value), sizeof(value));
+    return value;
+}
+
+// Read 2 bytes from the stream
+uint16_t read_s2be ()
+{
+    uint16_t value;
+    uint16_t result;
+    is.read(reinterpret_cast<char *>(&value), sizeof(value));
+    char *p = (char *)&value;
+    char tmp;
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+    tmp = p[0];
+    p[0] = p[1];
+    p[1] = tmp;
+#endif
+    result = value;
+    return result;
+}
+
+// Read 4 bytes from the stream
+uint32_t read_s4be ()
+{
+    uint32_t value;
+    is.read(reinterpret_cast<char *>(&value), sizeof(value));
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+    uint32_t result = 0;
+    result |= (value & 0x000000FF) << 24;
+    result |= (value & 0x0000FF00) << 8;
+    result |= (value & 0x00FF0000) >> 8;
+    result |= (value & 0xFF000000) >> 24;
+    value = result;
+#endif
+    return value;
+}
+
+// Read 8 bytes from the stream
+uint64_t read_s8be ()
+{
+    uint64_t value;
+    uint64_t result;
+    is.read(reinterpret_cast<char *>(&value), sizeof(value));
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+    char *p = (char *)&value;
+    char tmp;
+
+    tmp = p[0];
+    p[0] = p[7];
+    p[7] = tmp;
+
+    tmp = p[1];
+    p[1] = p[6];
+    p[6] = tmp;
+
+    tmp = p[2];
+    p[2] = p[5];
+    p[5] = tmp;
+
+    tmp = p[3];
+    p[3] = p[4];
+    p[4] = tmp;
+#endif
+    result = value;
+    return result;
+}
+
+void read_shape ()
+{
+    int32_t length = read_s4be();
+    cout << "\nReading shape pool and shape length, STRLEN = " << length;
+    std::vector<char> result(length);
+    is.read(&result[0], length);
+    cout << "\nData = " << std::string(result.begin(), result.end());
+}
+
+void read_package ()
+{
+  int32_t org = read_s4be();
+  int32_t name = read_s4be();
+  int32_t version = read_s4be();
+  cout << "\nReading package\n";
+  cout << "\nOrg = " << org;
+  cout << "\nName = " << name;
+  cout << "\nVersion = " << version;
+}
+
+void read_string ()
+{
+    uint32_t length = read_s4be();
+    cout << "\nReading string pool, STRLEN = " << length;
+    std::vector<char> result(length);
+    is.read(&result[0], length);
+    cout << "\nData = " << std::string(result.begin(), result.end());
+}
+
+void read_constant_pool() {
+    uint8_t ch = read_u1();
+    if (ch == TAG_ENUM_CP_ENTRY_STRING)
+        cout << "\nReading the constant pool tag = " << (tag_enum_t)ch;
+
+    switch ((tag_enum_t)ch) {
+      case TAG_ENUM_CP_ENTRY_STRING: {
+          read_string();
+          break;
+      }
+      case TAG_ENUM_CP_ENTRY_PACKAGE: {
+          read_package();
+          break;
+      }
+      case TAG_ENUM_CP_ENTRY_SHAPE: {
+          read_shape();
+          break;
+      }
+    }
+}
 
 /*
 struct BinTypeDecl {
@@ -81,43 +211,30 @@ struct BinBIRPackage {
     //vector<struct BinBIRFunction> functions;
 };
 
+struct BinBIRPackage readBIRPackage;
+
 class BIRReader {
   private:
   std::string   filename;
 
   public:
-    BIRReader();
-    BIRReader(std::string name); // This should open the file and initialize the
-                            // istream
-    ~BIRReader();
-
     std::string name()  { return filename; }
     char readByte();
     void deserialize() {
 
-        // Code copied from bir_t::constant_pool_set_t::_read() L90
-        // Purpose - read total pool count
-        m_constant_pool_count = m__io->read_s4be();
-        int l_constant_pool_entries = constant_pool_count();
-        m_constant_pool_entries = new std::vector<constant_pool_entry_t*>();
-        m_constant_pool_entries->reserve(l_constant_pool_entries);
-        for (int i = 0; i < l_constant_pool_entries; i++) {
-            m_constant_pool_entries->push_back(new constant_pool_entry_t(m__io, this, m__root));
+	is.open("main-bir-dump", ifstream::binary);
+
+        int32_t constant_pool_count;
+        constant_pool_count = read_s4be();
+        cout << "\nConstant Pool count = " << constant_pool_count;
+
+        for (int i = 0; i < constant_pool_count; i++) {
+            cout << "\nConstant pool entry = " << i;
+            read_constant_pool ();
         }
 
-        ifstream dataInStream;
-        int size = 0;
-        dataInStream.open("main-bir-dump", ios::in|ios::binary);
-        dataInStream.seekg(0, ios::end);
-        size = (int)dataInStream.tellg();
-        dataInStream.seekg (0, ios::beg);
-
-        // Read import packages.
-
-        readBIRPackage.org = m__io->read_s4be();
-        readBIRPackage.name = m__io->read_s4be();
-        readBIRPackage.value = m__io->read_s4be();
-        //void readPackages(dataInStream);
+        }
+        /*void readPackages(dataInStream);
 
         // Read constants.
         void readConstants(dataInStream);
@@ -145,7 +262,7 @@ class BIRReader {
       }
     }
 
-    /*void readFunctions(istream *dataInStream) {
+    void readFunctions(istream *dataInStream) {
       int symbolCount;
       dataInStream >> symbolCount;
       for (int i = 0; i < symbolCount; i++) {
--- BIR2llvmIR/driver.cpp
+++ BIR2llvmIR/driver.cpp
@@ -1,40 +1,7 @@
-//#include "BIRReader.h"
-#include<iostream>
-#include<string>
-#include<fstream>
-#include<vector>
-using namespace std;
+#include "BIRReader.h"
 
 int main()
 {
-	ifstream dataInStream;
-	//BIRReader *Reader = new BIRReader();
-
-        int size = 0;
-        dataInStream.open("main-bir-dump", ios::in|ios::binary);
-        dataInStream.seekg(0, ios::end);
-        size = (int)dataInStream.tellg();
-        dataInStream.seekg (0, ios::beg);
-
-	struct BinBIRPackage readBIRPackage;
-	char buf;
-	std::string org;
-        std::string name;
-        std::string value;
-
-	dataInStream.read((char*)&buf, sizeof(buf));
-	for (int i = 0; i < (int)buf; i++)
-		org += buf;
-	dataInStream.read((char*)&buf, sizeof(buf));
-	for (int i = 0; i < (int)buf; i++)
-		name += buf;
-	dataInStream.read((char*)&buf, sizeof(buf));
-	for (int i = 0; i < (int)buf; i++)
-		value += buf;
-
-	readBIRPackage.org = org;
-	readBIRPackage.name = name;
-	readBIRPackage.value = value;
-
-	//Reader.deserialize(Reader->dataInStream);
+	class BIRReader Reader;
+	Reader.deserialize();
 }
