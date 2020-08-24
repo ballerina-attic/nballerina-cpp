0956528e6bdb305709d42284f0389088aa48b72f
Ramakota Reddy <ramakota@rrlogic.co.in>
2020-08-24 20:28:48 +0530

    Able to use LLVM class members in our translation class with these changes.

--- BIR2llvmIR/llvmFunctionGen.cpp
+++ BIR2llvmIR/llvmFunctionGen.cpp
@@ -14,28 +14,29 @@ BIRFunction::BIRFunction(const BIRFunction&)
 {
 }
 
-void BIRFunction::translate (string modRef)
+void BIRFunction::translate (LLVMModuleRef modRef)
 {
+  cout <<"I am inside BIRFunction Translate Function\n";
   LLVMContext ctx;
-  //Module *M = new Module("MyModule", ctx);
+  Module *M = new Module("MyModule", ctx);
 
-  //FunctionType *FT =
-   // FunctionType::get(Type::getInt32Ty(ctx), /*not vararg*/false);
+  FunctionType *FT =
+    FunctionType::get(Type::getInt32Ty(ctx), /*not vararg*/false);
 
-  //Function *F = Function::Create(FT, Function::ExternalLinkage, "main", M);
+  Function *F = Function::Create(FT, Function::ExternalLinkage, "main", M);
 
 /*  BIRPackage pkg; // temp obj. how we will get pkg details?
   string ModuleName = pkg.org + pkg.name + pkg.version;
-  LLVMModuleRef mod = LLVMModuleCreateWithName(moduleID); // how we can call LLVM function ? 
+  LLVMModuleRef mod = LLVMModuleCreateWithName(moduleID); // how we can call LLVM function ?*/ 
 
-  LLVMTypeRef *retType = new LLVMTypeRef[returnVar]; // how we can use LLVMTypeRef types ?i how to generate LLVMTypeRe                                                     // using returne type.
+//  LLVMTypeRef *retType = new LLVMTypeRef[returnVar]; // how we can use LLVMTypeRef types ?i how to generate LLVMTypeRe                                                     // using returne type.
   LLVMTypeRef funcType;
   unsigned int numParams = paramCount; 
   bool isVarArg = false;
-  LLVMTypeRef *paramTypes  = new LLVMTypeRef[numParams]; // 
+  //LLVMTypeRef *paramTypes  = new LLVMTypeRef[numParams]; // 
   
 
-  for (unsigned i = 0;  i < numParams;  i++)
+/*  for (unsigned i = 0;  i < numParams;  i++)
     paramTypes[i] = // here, inside InvokableType class have paramType list. We have to get details from that list? 
 
   
