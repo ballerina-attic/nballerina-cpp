d2464de0dbf6e63736db5c21ccd6325d8663ffa8
shubhamnarlawar <shubham.narlawar@rrlogic.co.in>
2020-09-09 18:49:21 +0530

    Removed debug messages and moved type enum to BIR.h
    State of Code - Compilable

--- BIR2llvmIR/BIR.h
+++ BIR2llvmIR/BIR.h
@@ -36,7 +36,7 @@ enum SymbolKind {
 };
 
 enum VarKind {
-	LOCAL_VAR_KIND = 1,
+	LOCAL_VAR_KIND,
 	TEMP_VAR_KIND, 
 	RETURN_VAR_KIND, 
 	ARG_VAR_KIND, 
@@ -77,6 +77,61 @@ enum InstructionKind {
         BINARY_ADD,
         BINARY_DIV
 }; // we have to add more from bir-model.bal file
+
+enum type_tag_enum_t {
+        TYPE_TAG_ENUM_TYPE_TAG_INT = 1,
+        TYPE_TAG_ENUM_TYPE_TAG_BYTE = 2,
+        TYPE_TAG_ENUM_TYPE_TAG_FLOAT = 3,
+        TYPE_TAG_ENUM_TYPE_TAG_DECIMAL = 4,
+        TYPE_TAG_ENUM_TYPE_TAG_STRING = 5,
+        TYPE_TAG_ENUM_TYPE_TAG_BOOLEAN = 6,
+        TYPE_TAG_ENUM_TYPE_TAG_JSON = 7,
+        TYPE_TAG_ENUM_TYPE_TAG_XML = 8,
+        TYPE_TAG_ENUM_TYPE_TAG_TABLE = 9,
+        TYPE_TAG_ENUM_TYPE_TAG_NIL = 10,
+        TYPE_TAG_ENUM_TYPE_TAG_ANYDATA = 11,
+        TYPE_TAG_ENUM_TYPE_TAG_RECORD = 12,
+        TYPE_TAG_ENUM_TYPE_TAG_TYPEDESC = 13,
+        TYPE_TAG_ENUM_TYPE_TAG_STREAM = 14,
+        TYPE_TAG_ENUM_TYPE_TAG_MAP = 15,
+        TYPE_TAG_ENUM_TYPE_TAG_INVOKABLE = 16,
+        TYPE_TAG_ENUM_TYPE_TAG_ANY = 17,
+        TYPE_TAG_ENUM_TYPE_TAG_ENDPOINT = 18,
+        TYPE_TAG_ENUM_TYPE_TAG_ARRAY = 19,
+        TYPE_TAG_ENUM_TYPE_TAG_UNION = 20,
+        TYPE_TAG_ENUM_TYPE_TAG_INTERSECTION = 21,
+        TYPE_TAG_ENUM_TYPE_TAG_PACKAGE = 22,
+        TYPE_TAG_ENUM_TYPE_TAG_NONE = 23,
+        TYPE_TAG_ENUM_TYPE_TAG_VOID = 24,
+        TYPE_TAG_ENUM_TYPE_TAG_XMLNS = 25,
+        TYPE_TAG_ENUM_TYPE_TAG_ANNOTATION = 26,
+        TYPE_TAG_ENUM_TYPE_TAG_SEMANTIC_ERROR = 27,
+        TYPE_TAG_ENUM_TYPE_TAG_ERROR = 28,
+        TYPE_TAG_ENUM_TYPE_TAG_ITERATOR = 29,
+        TYPE_TAG_ENUM_TYPE_TAG_TUPLE = 30,
+        TYPE_TAG_ENUM_TYPE_TAG_FUTURE = 31,
+        TYPE_TAG_ENUM_TYPE_TAG_FINITE = 32,
+        TYPE_TAG_ENUM_TYPE_TAG_OBJECT = 33,
+        TYPE_TAG_ENUM_TYPE_TAG_SERVICE = 34,
+        TYPE_TAG_ENUM_TYPE_TAG_BYTE_ARRAY = 35,
+        TYPE_TAG_ENUM_TYPE_TAG_FUNCTION_POINTER = 36,
+        TYPE_TAG_ENUM_TYPE_TAG_HANDLE = 37,
+        TYPE_TAG_ENUM_TYPE_TAG_READONLY = 38,
+        TYPE_TAG_ENUM_TYPE_TAG_SIGNED32_INT = 39,
+        TYPE_TAG_ENUM_TYPE_TAG_SIGNED16_INT = 40,
+        TYPE_TAG_ENUM_TYPE_TAG_SIGNED8_INT = 41,
+        TYPE_TAG_ENUM_TYPE_TAG_UNSIGNED32_INT = 42,
+        TYPE_TAG_ENUM_TYPE_TAG_UNSIGNED16_INT = 43,
+        TYPE_TAG_ENUM_TYPE_TAG_UNSIGNED8_INT = 44,
+        TYPE_TAG_ENUM_TYPE_TAG_CHAR_STRING = 45,
+        TYPE_TAG_ENUM_TYPE_TAG_XML_ELEMENT = 46,
+        TYPE_TAG_ENUM_TYPE_TAG_XML_PI = 47,
+        TYPE_TAG_ENUM_TYPE_TAG_XML_COMMENT = 48,
+        TYPE_TAG_ENUM_TYPE_TAG_XML_TEXT = 49,
+        TYPE_TAG_ENUM_TYPE_TAG_NEVER = 50,
+        TYPE_TAG_ENUM_TYPE_TAG_NULL_SET = 51,
+        TYPE_TAG_ENUM_TYPE_TAG_PARAMETERIZED_TYPE = 52
+};
 
 enum VarScope {
 	VAR_SCOPE_GLOBAL=1, 
@@ -299,15 +354,11 @@ class AbstractInsn: public BIRNode {
 
 class NonTerminatorInsn: public AbstractInsn {
   private:
-   // BIRFunction       *BFunc;
 
   public:
     NonTerminatorInsn();
     NonTerminatorInsn(Location *pos, InstructionKind kind, Operand *lOp);
     ~NonTerminatorInsn();
-
-   // void          setFunction(BIRFunction *func) { BFunc = func; }
-   // BIRFunction * getFunction()                  { return BFunc; }
 
     void translate(LLVMModuleRef &modRef);
 };
@@ -322,11 +373,11 @@ class TerminatorInsn: public AbstractInsn {
 				 BasicBlockT *then);
     ~TerminatorInsn();
 
-    BasicBlockT * getNextBB()                    { return thenBB; }
+    BasicBlockT * getNextBB()           { return thenBB; }
     bool getPatchStatus()                      { return patchRequire; }
 
+    void setNextBB(BasicBlockT *block)  { thenBB = block; }
     void setPatchStatus(bool patchrequire)     { patchRequire = patchrequire; }
-    void         setNextBB(BasicBlockT *block) { thenBB = block; }
 
     void translate(LLVMModuleRef &modRef);
 };
@@ -381,14 +432,19 @@ class BinaryOpInsn : public NonTerminatorInsn {
     void translate(LLVMModuleRef &modRef);
 };
 
-class GoToInsn : public NonTerminatorInsn {
+class GoToInsn : public TerminatorInsn {
   private:
+    LLVMValueRef llvmInsn;
 
   public:
     GoToInsn();
+    GoToInsn(Location *pos, InstructionKind kind, Operand *lOp,
+             BasicBlockT *nextBB);
     ~GoToInsn();
 
-    void translate(LLVMModuleRef &modRef);
+    LLVMValueRef getLLVMInsn()           { return llvmInsn; }
+    void setLLVMInsn(LLVMValueRef insn)  { llvmInsn = insn; }
+    void translat(LLVMModuleRef &modRef);
 };
 
 class ReturnInsn : public TerminatorInsn {
@@ -400,7 +456,7 @@ class ReturnInsn : public TerminatorInsn {
 		 BasicBlockT *nextBB);
     ~ReturnInsn();
 
-    void translate(LLVMModuleRef &modRef);
+    void translat(LLVMModuleRef &modRef);
 };
 
 class BasicBlockT: public BIRNode {
@@ -408,9 +464,10 @@ class BasicBlockT: public BIRNode {
     string                       id;
     vector<NonTerminatorInsn *>  instructions;
     TerminatorInsn              *terminator;
-    LLVMBuilderRef 	        BRef;
+    LLVMBuilderRef 	         BRef;
     BIRFunction                 *BFunc;
-    BasicBlockT                  *nextBB;
+    BasicBlockT                 *nextBB;
+    LLVMBasicBlockRef            bbRefObj;
 
   public:
     BasicBlockT();
@@ -422,16 +479,18 @@ class BasicBlockT: public BIRNode {
     TerminatorInsn * getTerminatorInsn() { return terminator; }
     LLVMBuilderRef   getLLVMBuilderRef() { return BRef; }
     BIRFunction    * getBIRFunction()    { return BFunc; }
-    BasicBlockT     * getNextBB()         { return nextBB; }
+    BasicBlockT     * getNextBB()        { return nextBB; }
     vector<NonTerminatorInsn *> getNonTerminatorInsn()       { return instructions; }
     NonTerminatorInsn *         getInsn(int i) { return instructions[i]; }
+    LLVMBasicBlockRef getLLVMBBRef()     { return bbRefObj; }
 
-    void setId(string newId)                     { id = newId; }
-    void setTerminatorInsn(TerminatorInsn *insn) { terminator = insn; }
-    void setLLVMBuilderRef(LLVMBuilderRef buildRef) {BRef = buildRef; } 
-    void setBIRFunction(BIRFunction *func)       { BFunc = func; }
-    void setNextBB(BasicBlockT *bb)               { nextBB = bb; }
-    void addNonTermInsn(NonTerminatorInsn *insn)  { 
+    void setId(string newId)                        { id = newId; }
+    void setTerminatorInsn(TerminatorInsn *insn)    { terminator = insn; }
+    void setLLVMBuilderRef(LLVMBuilderRef buildRef) { BRef = buildRef; } 
+    void setBIRFunction(BIRFunction *func)          { BFunc = func; }
+    void setNextBB(BasicBlockT *bb)                 { nextBB = bb; }
+    void setLLVMBBRef(LLVMBasicBlockRef bbRef)      { bbRefObj = bbRef; }
+    void addNonTermInsn(NonTerminatorInsn *insn)    { 
 			instructions.push_back(insn); }
 
     void translate(LLVMModuleRef &modRef);
@@ -595,7 +654,7 @@ class BIRFunction: public BIRNode {
     void setLocalVar(VarDecl *var)          { localVars.push_back(var); }
     void setReturnVar(VarDecl *var)         { returnVar = var; }
     void setBasicBlockTs(vector<BasicBlockT *> b) { basicBlocks = b; }
-    void setBasicBlockT(BasicBlockT *bb)      { basicBlocks.push_back(bb); }
+    void addBasicBlockT(BasicBlockT *bb)    { basicBlocks.push_back(bb); }
     void setWorkerName(string newName)      { workerName = newName; }
     void setLLVMBuilder(LLVMBuilderRef b)  { builder = b; }
     void setLocalVarRefs(map<string, LLVMValueRef> newLocalVarRefs)  {
@@ -606,7 +665,8 @@ class BIRFunction: public BIRNode {
     LLVMValueRef  getLocalVarRefUsingId(string locVar);
     LLVMValueRef  getLocalToTempVar(Operand *operand);
     void          translateFunctionBody(LLVMModuleRef &modRef);
-
+    void	  patchInsn(Function* llvnFun);
+    LLVMTypeRef   getLLVMFuncRetTypeRefOfType(TypeDecl *typeD);
     void translate(LLVMModuleRef &modRef);
 };
 
@@ -641,7 +701,7 @@ class BIRPackage: public BIRNode {
     void setFunctions(vector<BIRFunction *> f)  { functions = f; }
     void addFunction(BIRFunction * f)           { functions.push_back(f); }
 
-    void translate();
+    void translate(LLVMModuleRef &modRef);
 };
 
 #endif // BIR_H
