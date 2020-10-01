b29dcf487afc3f3e87ffe8a571ccc9a24f3f2c02
Ramakota Reddy <ramakota@rrlogic.co.in>
2020-10-01 08:18:30 +0530

    Code changes for if condition and while loop. Added Code for Global Variables and Unary Operations.
    Able to dump the IR with if and While branch conditions.

--- BIR2llvmIR/llvmConstantLoadInsn.cpp
+++ BIR2llvmIR/llvmConstantLoadInsn.cpp
@@ -14,12 +14,37 @@ ConstantLoadInsn::~ConstantLoadInsn() {
 }
 
 void ConstantLoadInsn::translate(LLVMModuleRef &modRef) {
-  LLVMBuilderRef builder = getFunction()->getLLVMBuilder();
-  
-  LLVMValueRef lhsRef    = getFunction()->getLocalVarRefUsingId(
-                            getLhsOperand()->getVarDecl()->getVarName());
+  LLVMBuilderRef builder;   
+  LLVMValueRef   lhsRef;
+  LLVMValueRef   constRef;
+  if (getFunction() && getLhsOperand() && 
+	getLhsOperand()->getVarDecl()) {
+    builder = getFunction()->getLLVMBuilder();
 
-  LLVMValueRef constRef  = LLVMConstInt(LLVMInt32Type(), value, 0);
+    lhsRef  = getFunction()->getLocalVarRefUsingId(
+                            getLhsOperand()->getVarDecl()->getVarName());
+  }
+
+  if (getLhsOperand() && getLhsOperand()->getVarDecl() &&
+       getLhsOperand()->getVarDecl()->getTypeDecl()) {
+    int typeTag = getLhsOperand()->getVarDecl()->getTypeDecl()->getTypeTag();
+    switch (typeTag) {
+      case TYPE_TAG_ENUM_TYPE_TAG_INT:
+      {
+        constRef = LLVMConstInt(LLVMInt32Type(), value, 0);
+	break;
+      }
+      case TYPE_TAG_ENUM_TYPE_TAG_BYTE:
+      case TYPE_TAG_ENUM_TYPE_TAG_FLOAT:
+      case TYPE_TAG_ENUM_TYPE_TAG_BOOLEAN:
+      {
+        constRef = LLVMConstInt(LLVMInt8Type(), value, 0);
+	break;
+      }
+      default:
+        constRef = LLVMConstInt(LLVMInt32Type(), value, 0);
+    }
+  }
 
   LLVMBuildStore(builder, constRef, lhsRef);
 }
