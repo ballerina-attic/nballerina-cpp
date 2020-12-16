638fa5ff9b6d61c3601a28ff0dc7af2c4df14edb
shubhamnarlawar <shubham.narlawar@rrlogic.co.in>
2020-12-16 14:34:00 +0530

    Add print routines in Rust Library

--- BIR2llvmIR/strLibrary.rs
+++ BIR2llvmIR/strLibrary.rs
@@ -1,5 +1,6 @@
-// String comparision implementation in Rust
+// Rust Library
 
+// String comparision
 fn is_same_type(src_type : &str, dest_type : &str) -> bool
 {
   let same_type:bool;
@@ -11,4 +12,70 @@ fn is_same_type(src_type : &str, dest_type : &str) -> bool
     same_type = false;
   }
   return same_type;
+}
+
+// Prints 64 bit signed integer
+fn print64(num64 : i64)
+{
+   println!("{}", num64);
+}
+
+// Prints 32 bit signed integer
+fn print32(num32 : i32)
+{
+   println!("{}", num32);
+}
+
+// Prints 16 bit signed integer
+fn print16(num16 : i16)
+{
+   println!("{}", num16);
+}
+
+// Prints 8 bit signed integer
+fn print8(num8 : i8)
+{
+   println!("{}", num8);
+}
+
+// Prints 64 bit unsigned integer
+fn printu64(num64 : u64)
+{
+   println!("{}", num64);
+}
+
+// Prints 32 bit unsigned integer
+fn printu32(num32 : u32)
+{
+   println!("{}", num32);
+}
+
+// Prints 16 bit unsigned integer
+fn printu16(num16 : u16)
+{
+   println!("{}", num16);
+}
+
+// Prints 8 bit unsigned integer
+fn printu8(num8 : u8)
+{
+   println!("{}", num8);
+}
+
+// Prints 64 bit float
+fn printf64(num64 : f64)
+{
+   println!("{}", num64);
+}
+
+// Prints 32 bit float
+fn printf32(num32 : f32)
+{
+   println!("{}", num32);
+}
+
+// Prints string
+fn print_str(string : &str)
+{
+   println!("{}", string);
 }
