0b7f1f462093d63a70bb9853f9d32656b4e05e77
Ruvinda Dhambarage <ruvinda@wso2.com>
2021-02-16 12:22:36 +0530

    Check invariants and generate new map call

--- compiler/llvmStructureInsn.cpp
+++ compiler/llvmStructureInsn.cpp
@@ -3,14 +3,33 @@
 void StructureInsn::translate(LLVMModuleRef &modRef) {
   BIRFunction *funcObj = getFunction();
   BIRPackage *pkgObj = getPkgAddress();
-  string lhsName = getLhsOperand()->name();
   LLVMBuilderRef builder = funcObj->getLLVMBuilder();
 
+  string lhsName = getLhsOperand()->name();
   LLVMValueRef lhsOpRef = funcObj->getLocalVarRefUsingId(lhsName);
   if (!lhsOpRef)
     lhsOpRef = pkgObj->getGlobalVarRefUsingId(lhsName);
 
-  LLVMValueRef newStructFunc = pkgObj->getFunctionRefBasedOnName("ruvi");
+  // to optimize; early return and in variant checking
+  int memberTypeTag = 0;
+  auto vars = funcObj->getLocalVars();
+  for (auto var : vars) {
+    if (var->getVarName() == lhsName) {
+      assert(var->getVarKind() == LOCAL_VAR_KIND);
+      assert(var->getTypeDecl()->getTypeTag() == TYPE_TAG_MAP);
+      MapTypeDecl *mapTypeDelare =
+          static_cast<MapTypeDecl *>(var->getTypeDecl());
+      memberTypeTag = mapTypeDelare->getTypeMemberTag();
+      break;
+    }
+  }
+  assert(memberTypeTag);
+
+  if (memberTypeTag != TYPE_TAG_INT)
+    std::cerr << "Non init type maps are currently not supported" << std::endl;
+
+  LLVMValueRef newStructFunc =
+      pkgObj->getFunctionRefBasedOnName("new_map_init");
   if (!newStructFunc)
     newStructFunc = getNewStructureDeclaration(modRef, pkgObj);
   assert(newStructFunc);
@@ -23,7 +42,7 @@ void StructureInsn::translate(LLVMModuleRef &modRef) {
 LLVMValueRef StructureInsn::getNewStructureDeclaration(LLVMModuleRef &modRef,
                                                        BIRPackage *pkg) {
   LLVMTypeRef funcType = LLVMFunctionType(LLVMInt32Type(), nullptr, 0, 0);
-  LLVMValueRef addedFuncRef = LLVMAddFunction(modRef, "ruvi", funcType);
-  pkg->addArrayFunctionRef("ruvi", addedFuncRef);
+  LLVMValueRef addedFuncRef = LLVMAddFunction(modRef, "new_map_init", funcType);
+  pkg->addArrayFunctionRef("new_map_init", addedFuncRef);
   return addedFuncRef;
 }
