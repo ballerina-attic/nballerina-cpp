d04a02003911958c3d88741bb3d05d77229d7da2
Ruvinda Dhambarage <ruvinda@wso2.com>
2021-02-16 14:31:56 +0530

    Codegen for map store int

--- compiler/llvmStructureInsn.cpp
+++ compiler/llvmStructureInsn.cpp
@@ -28,8 +28,7 @@ void StructureInsn::translate(LLVMModuleRef &modRef) {
   if (memberTypeTag != TYPE_TAG_INT)
     std::cerr << "Non init type maps are currently not supported" << std::endl;
 
-  LLVMValueRef newStructFunc =
-      pkgObj->getFunctionRefBasedOnName("new_map_init");
+  LLVMValueRef newStructFunc = pkgObj->getFunctionRefBasedOnName("map_new_int");
   if (!newStructFunc)
     newStructFunc = getNewStructureDeclaration(modRef, pkgObj);
   assert(newStructFunc);
@@ -42,8 +41,8 @@ void StructureInsn::translate(LLVMModuleRef &modRef) {
 LLVMValueRef StructureInsn::getNewStructureDeclaration(LLVMModuleRef &modRef,
                                                        BIRPackage *pkg) {
   LLVMTypeRef funcType = LLVMFunctionType(LLVMInt32Type(), nullptr, 0, 0);
-  LLVMValueRef addedFuncRef = LLVMAddFunction(modRef, "new_map_init", funcType);
-  pkg->addArrayFunctionRef("new_map_init", addedFuncRef);
+  LLVMValueRef addedFuncRef = LLVMAddFunction(modRef, "map_new_int", funcType);
+  pkg->addArrayFunctionRef("map_new_int", addedFuncRef);
   return addedFuncRef;
 }
 
@@ -52,6 +51,42 @@ MapStoreInsn::MapStoreInsn(Location *pos, InstructionKind kind, Operand *lOp,
     : NonTerminatorInsn(pos, kind, lOp), keyOp(KOp), rhsOp(rOp) {}
 
 LLVMValueRef MapStoreInsn::getMapStoreDeclaration(LLVMModuleRef &modRef,
-                                                      BIRPackage *pkg) {}
+                                                  BIRPackage *pkg) {
+  LLVMTypeRef *paramTypes = new LLVMTypeRef[3];
+  LLVMTypeRef int32PtrType = LLVMPointerType(LLVMInt32Type(), 0);
+  LLVMTypeRef charArrayPtrType = LLVMPointerType(LLVMInt8Type(), 0);
+  paramTypes[0] = int32PtrType;
+  paramTypes[1] = charArrayPtrType;
+  paramTypes[2] = int32PtrType;
+  LLVMTypeRef funcType = LLVMFunctionType(LLVMVoidType(), paramTypes, 3, 0);
+  LLVMValueRef addedFuncRef =
+      LLVMAddFunction(modRef, "map_store_int", funcType);
+  pkg->addArrayFunctionRef("map_store_int", addedFuncRef);
+  return addedFuncRef;
+}
 
-void MapStoreInsn::translate(LLVMModuleRef &modRef) {}
+void MapStoreInsn::translate(LLVMModuleRef &modRef) {
+  BIRFunction *funcObj = getFunction();
+  BIRPackage *pkgObj = getPkgAddress();
+  string lhsName = getLhsOperand()->name();
+  string rhsName = rhsOp->name();
+  LLVMBuilderRef builder = funcObj->getLLVMBuilder();
+  LLVMValueRef mapStoreFunc =
+      pkgObj->getFunctionRefBasedOnName("map_store_int");
+  if (!mapStoreFunc)
+    mapStoreFunc = getMapStoreDeclaration(modRef, pkgObj);
+
+  LLVMValueRef lhsOpRef = funcObj->getLocalVarRefUsingId(lhsName);
+  if (!lhsOpRef)
+    lhsOpRef = pkgObj->getGlobalVarRefUsingId(lhsName);
+
+  LLVMValueRef rhsOpRef = funcObj->getLocalVarRefUsingId(rhsName);
+  LLVMValueRef keyRef = funcObj->getLocalToTempVar(keyOp);
+  assert(mapStoreFunc && lhsOpRef && rhsOpRef && keyRef);
+  LLVMValueRef *argOpValueRef = new LLVMValueRef[3];
+  argOpValueRef[0] = lhsOpRef;
+  argOpValueRef[1] = keyRef;
+  argOpValueRef[2] = rhsOpRef;
+
+  LLVMBuildCall(builder, mapStoreFunc, argOpValueRef, 3, "");
+}
