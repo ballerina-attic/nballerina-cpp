fdb2ea03d6df5f2090b01103d5d9cf7a60374fa7
Ruvinda Dhambarage <ruvinda@wso2.com>
2021-02-16 14:55:11 +0530

    Add Runtime functionality

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -19,84 +19,78 @@
 
 // Rust Library
 
+use std::ffi::CStr;
 use std::mem;
 use std::os::raw::c_char;
+use std::slice;
+
+mod bal_map;
+pub use bal_map::map::BalMapInt;
 
 // String comparision
 #[no_mangle]
-pub extern "C" fn is_same_type(src_type : *const c_char, dest_type : *const c_char) -> bool
-{
-  return src_type == dest_type;
+pub extern "C" fn is_same_type(src_type: *const c_char, dest_type: *const c_char) -> bool {
+    return src_type == dest_type;
 }
 
 // Prints 64 bit signed integer
 #[no_mangle]
-pub extern "C" fn print64(num64 : i64)
-{
-   println!("{}", num64);
+pub extern "C" fn print64(num64: i64) {
+    println!("{}", num64);
 }
 
 // Prints 32 bit signed integer
 #[no_mangle]
-pub extern "C" fn print32(num32 : i32)
-{
-   println!("{}", num32);
+pub extern "C" fn print32(num32: i32) {
+    println!("{}", num32);
 }
 
 // Prints 16 bit signed integer
 #[no_mangle]
-pub extern "C" fn print16(num16 : i16)
-{
-   println!("{}", num16);
+pub extern "C" fn print16(num16: i16) {
+    println!("{}", num16);
 }
 
 // Prints 8 bit signed integer
 #[no_mangle]
-pub extern "C" fn print8(num8 : i8)
-{
-   println!("{}", num8);
+pub extern "C" fn print8(num8: i8) {
+    println!("{}", num8);
 }
 
 // Prints 64 bit unsigned integer
 #[no_mangle]
-pub extern "C" fn printu64(num64 : u64)
-{
-   println!("{}", num64);
+pub extern "C" fn printu64(num64: u64) {
+    println!("{}", num64);
 }
 
 // Prints 32 bit unsigned integer
 #[no_mangle]
-pub extern "C" fn printu32(num32 : u32)
-{
-   println!("{}", num32);
+pub extern "C" fn printu32(num32: u32) {
+    println!("{}", num32);
 }
 
 // Prints 16 bit unsigned integer
 #[no_mangle]
-pub extern "C" fn printu16(num16 : u16)
-{
-   println!("{}", num16);
+pub extern "C" fn printu16(num16: u16) {
+    println!("{}", num16);
 }
 
 // Prints 8 bit unsigned integer
 #[no_mangle]
-pub extern "C" fn printu8(num8 : u8)
-{
-   println!("{}", num8);
+pub extern "C" fn printu8(num8: u8) {
+    println!("{}", num8);
 }
 
 // Prints 64 bit float
 #[no_mangle]
-pub extern "C" fn printf64(num64 : f64)
-{
-   println!("{}", num64);
+pub extern "C" fn printf64(num64: f64) {
+    println!("{}", num64);
 }
 
 // Prints 32 bit float
 #[no_mangle]
-pub extern "C" fn printf32(num32 : f32)
-{
-   println!("{}", num32);
+pub extern "C" fn printf32(num32: f32) {
+    println!("{}", num32);
 }
 
 #[no_mangle]
@@ -125,9 +119,51 @@ pub extern "C" fn int_array_store(arr_ptr: *mut Vec<*mut i32>, n: i32, ref_ptr: 
 
 #[no_mangle]
 pub extern "C" fn int_array_load(arr_ptr: *mut Vec<*mut i32>, n: i32) -> *mut i32 {
-    let arr = unsafe { Box::from_raw(arr_ptr)};
+    let arr = unsafe { Box::from_raw(arr_ptr) };
     let n_size = n as usize;
     let return_val = arr[n_size];
     mem::forget(arr);
     return return_val;
+}
+
+// Ballerina Map implementation
+#[no_mangle]
+pub extern "C" fn map_new_int() -> *mut BalMapInt {
+    Box::into_raw(Box::new(BalMapInt::new()))
+}
+
+#[no_mangle]
+pub extern "C" fn map_deint_int(ptr: *mut BalMapInt) {
+    if ptr.is_null() {
+        return;
+    }
+    unsafe {
+        Box::from_raw(ptr);
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn map_store_int(ptr: *mut BalMapInt, key: *const c_char, member_ptr: *const i32) {
+    // Load BalMap from pointer
+    let bal_map = unsafe {
+        assert!(!ptr.is_null());
+        &mut *ptr
+    };
+    // Load Key C string
+    let key = unsafe {
+        assert!(!key.is_null());
+        CStr::from_ptr(key)
+    };
+    let key_str = key.to_str().unwrap();
+
+    // Load member value
+    let member = unsafe {
+        assert!(!member_ptr.is_null());
+        slice::from_raw_parts(member_ptr, 1)
+    };
+    // Insert new field
+    bal_map.insert(key_str, member[0]);
+
+    // Print length to test functionality
+    println!("length={}", bal_map.length());
 }
