faa4f9e08b5cb6990c765bb253d4737eb2990f26
shubhamnarlawar <shubham.narlawar@rrlogic.co.in>
2021-03-15 11:49:18 +0530

    Fix issue #175

--- test/testRunScript.sh
+++ test/testRunScript.sh
@@ -9,7 +9,7 @@ then
   exit 1
 fi
 
-ballerina build --dump-bir-file=$filename-bir-dump $1 1>out.log 2>err.log
+ballerina build --dump-bir-file=$filename-bir-dump $1 1>bal_out.log 2>bal_err.log
 
 #Ignoring Ballerina compiler errors due to the use of undefined external functions for print routines
 #if [ -s ./err.log ]
@@ -18,19 +18,21 @@ ballerina build --dump-bir-file=$filename-bir-dump $1 1>out.log 2>err.log
 #  exit 1
 #fi
 
-$2 $filename-bir-dump  2>err.log
+$2 $filename-bir-dump  2>nbal_err.log
 
-if [ -s ./err.log ]
+if [ -s ./nbal_err.log ]
 then
-  echo "nballerinacc error. Check err.log"
+  echo "nballerinacc error. Error msg: "
+  echo ./nbal_err.log
   exit 1
 fi
 
-clang -O0 -o $filename.out $filename-bir-dump.ll -L../../../runtime/target/release -lballerina_rt 2>err.log
+clang -O0 -o $filename.out $filename-bir-dump.ll -L../../../runtime/target/release/ -lballerina_rt 2>clang_err.log
 
-if [ -s ./err.log ]
+if [ -s ./clang_err.log ]
 then
-  echo "clang error/warning. Check err.log"
+  echo "clang error/warning. Error msg: "
+  echo ./clang_err.log
   exit 1
 fi
 
